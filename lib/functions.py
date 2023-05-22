9e48c2bbeddaa86e1ff6b8bc0c2ef97faa5c9b93
Sarah Dean <128323898+sarahadean@users.noreply.github.com>
2023-05-22 14:54:26 -0500

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print('Hello, programmer!\n\n')
 
 def greet(name):
-    pass
+    print(f'Hello, {name}\n')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f'Hello, {name}')
 
 def add(num1, num2):
-    pass
+    return num1 + num2
 
 def halve(number):
     pass
